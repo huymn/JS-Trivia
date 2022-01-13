da5bf304e59115797be9854a3cbef60aed2e030c
nguyenhu21 <35584482+huymn@users.noreply.github.com>
2022-01-12 19:41:21 -0800

    added express server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const router = express.Router()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', router);
+app.listen(process.env.PORT || 3000)
+
+console.log(`Server running on Port: ${PORT}`)
