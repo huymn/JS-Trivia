812f31e7f6267321bda55a18097e5ba35bf83be1
nguyenhu21 <35584482+huymn@users.noreply.github.com>
2022-01-12 19:57:01 -0800

    Added about page content

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'));
 })
 
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname+'/about.html'));
+})
+
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', router);
