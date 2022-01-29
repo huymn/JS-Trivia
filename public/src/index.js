6226b138a605df7915c696c520686b8d8c2db8c5
nguyenhu21 <35584482+huymn@users.noreply.github.com>
2022-01-28 20:52:37 -0800

    added update to question and choices if button is clicked

--- public/src/index.js
+++ public/src/index.js
@@ -5,20 +5,45 @@ const secondOption = document.getElementById('B')
 const thirdOption = document.getElementById('C')
 const fourthOption = document.getElementById('D')
 
+//Current question number that the user is on (range is from 0-9 so 10 questions in total)
+let currQuestionNum = 0;
+
+//Async function to fetch the data from the API
 async function fetchData() {
     let response = await fetch(triviaEasyUrl)
     let data = await response.json()
     return data.results
-    
 }
 
+//Main function for the game
 fetchData().then((res) => {
-    console.log(res[0])
-    questionHeader.innerHTML = res[0].question
-    firstOption.innerHTML = res[0].correct_answer
-    secondOption.innerHTML = res[0].incorrect_answers[0]
-    thirdOption.innerHTML = res[0].incorrect_answers[1]
-    fourthOption.innerHTML = res[0].incorrect_answers[2]
-
+    console.log(res[currQuestionNum])
+    questionHeader.innerHTML = res[currQuestionNum].question
+    firstOption.innerHTML = res[currQuestionNum].correct_answer
+    secondOption.innerHTML = res[currQuestionNum].incorrect_answers[0]
+    thirdOption.innerHTML = res[currQuestionNum].incorrect_answers[1]
+    fourthOption.innerHTML = res[currQuestionNum].incorrect_answers[2]
+    firstOption.addEventListener('click', () => {
+        if(checkAnswer(res[currQuestionNum].correct_answer, res[currQuestionNum].correct_answer)) {
+            currQuestionNum++;
+            console.log("clicked");
+            update(res[currQuestionNum].question, res[currQuestionNum].correct_answer, res[currQuestionNum].incorrect_answers)
+        }
+    })
 })
+
+//Check if the selected answer by the user is correct
+const checkAnswer = (choice, answer) => {
+    return choice === answer
+}
+
+
+//Update the page with new questions and set of answers
+const update = (question, correctAns, wrongAns) => {
+    questionHeader.innerHTML = question
+    firstOption.innerHTML = correctAns
+    secondOption.innerHTML = wrongAns[0]
+    thirdOption.innerHTML = wrongAns[1]
+    fourthOption.innerHTML = wrongAns[3]
+}
 
