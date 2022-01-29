5c864e57da13b182c426179d73989aaa79f6d1ae
nguyenhu21 <35584482+huymn@users.noreply.github.com>
2022-01-28 20:28:54 -0800

    added answer choices

--- public/src/index.js
+++ public/src/index.js
@@ -1,5 +1,9 @@
 const triviaEasyUrl = 'https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple'
 const questionHeader = document.getElementById('question')
+const firstOption = document.getElementById('A')
+const secondOption = document.getElementById('B')
+const thirdOption = document.getElementById('C')
+const fourthOption = document.getElementById('D')
 
 async function fetchData() {
     let response = await fetch(triviaEasyUrl)
@@ -9,8 +13,12 @@ async function fetchData() {
 }
 
 fetchData().then((res) => {
-    console.log(res[0].question)
+    console.log(res[0])
     questionHeader.innerHTML = res[0].question
+    firstOption.innerHTML = res[0].correct_answer
+    secondOption.innerHTML = res[0].incorrect_answers[0]
+    thirdOption.innerHTML = res[0].incorrect_answers[1]
+    fourthOption.innerHTML = res[0].incorrect_answers[2]
 
 })
 
