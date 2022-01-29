98a5c2fe9d2004f267f303a00e011784fc30f43a
nguyenhu21 <35584482+huymn@users.noreply.github.com>
2022-01-28 20:22:58 -0800

    got question to display

--- public/src/index.js
+++ public/src/index.js
@@ -1,11 +1,16 @@
 const triviaEasyUrl = 'https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple'
-const questionHeader = document.getElementById('question');
+const questionHeader = document.getElementById('question')
 
-async function fetchText() {
+async function fetchData() {
     let response = await fetch(triviaEasyUrl)
-    let data = await response.json();
-    console.log(data.results[1].question);
-    questionHeader.innerHTML = data.results[1].question
+    let data = await response.json()
+    return data.results
+    
 }
 
-fetchText();
+fetchData().then((res) => {
+    console.log(res[0].question)
+    questionHeader.innerHTML = res[0].question
+
+})
+
